fab9677035b14eaa7315453fd9de9321f8bc9d96
j-goodman <johngoodman@pursuit.org>
2023-08-12 12:01:02 -0400

    Add routes to update and delete articles.

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -3,7 +3,9 @@ const articles = express.Router();
 const {
   getAllArticles,
   getArticle,
+  deleteArticle,
   getArticleByTitle,
+  updateArticle,
   newArticle,
 } = require("../queries/articles");
 
@@ -28,13 +30,42 @@ articles.get("/:title", async (req, res) => {
   }
 });
 
+const validateArticle = (req, res, next) => {
+  const { title } = req.body;
+  if (
+    title[title.length - 1] !== "?"
+  ) {
+    next();
+  } else {
+    res.status(400).json({ error: "Article title may not end in a question mark." });
+  }
+};
+
 // CREATE
-articles.post("/", async (req, res) => {
+articles.post("/", validateArticle, async (req, res) => {
   try {
     const article = await newArticle(req.body);
     res.json(article);
   } catch (error) {
     res.status(400).json({ error: error });
+  }
+});
+
+// UPDATE
+articles.put("/:id", validateArticle, async (req, res) => {
+  const { id } = req.params;
+  const updatedArticle = await updateArticle(id, req.body);
+  res.status(200).json(updatedArticle);
+});
+
+// DELETE
+articles.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedArticle = await deleteArticle(id);
+  if (deletedArticle.id) {
+    res.status(200).json(deletedArticle);
+  } else {
+    res.status(404).json("Bookmark not found");
   }
 });
 
